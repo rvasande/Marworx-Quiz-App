03a94a170246b5c60e263db060f2ff87002e9711
rushikesh <rushikeshvasande333@gmail.com>
2024-07-17 19:14:53 +0530

    global error handling and users api

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
-// const globalErrorHandler = require("./controller/errorController");
-// const AppError = require('./utils/appError')
+const globalErrorHandler = require("./controller/errorController");
+const AppError = require('./utils/appError')
 const cors = require("cors");
+const userRoute = require('./route/userRoute')
 
 const app = express();
 
 app.use(cors());
 
+app.use('/api/v1/users',userRoute)
 
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
+});
 
-// app.all("*", (req, res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
-// });
-
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ backend/route/userRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userController = require('../controller/userController')
+const router = express.Router()
+
+router.route('/')
+
+
+module.exports = router;
